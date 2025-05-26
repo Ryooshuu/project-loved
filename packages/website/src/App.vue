b3deb92c1b55e3f941dbebec46524f0404adc9fe
Ryooshuu <kyoko.laica@gmail.com>
2025-05-26 13:16:51 -0400

    separate navbar element in website to its own component

--- packages/website/src/App.vue
+++ packages/website/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { routes } from "vue-router/auto-routes";
-import { IconoirProvider, Check, UserLove } from "@iconoir/vue";
+import { IconoirProvider, Check } from "@iconoir/vue";
 import { motion } from "motion-v";
-import { Navbar } from "@loved/ui";
+import LovedNavbar from "./components/LovedNavbar.vue";
 
 console.log(`There are ${routes.length} routes.`);
 
@@ -18,50 +18,7 @@ const count = ref(0);
         }"
     >
         <div class="app font-sans">
-            <Navbar.Root>
-                <Navbar.Section
-                    :logo-inset="{
-                        left: 15,
-                        top: 32.5,
-                        bottom: 9.5
-                    }"
-                >
-                    <template #logo>
-                        <p class="text-xl mr tracking-[3px] select-none glow max-lg:hidden">
-                            PROJECT LOVED
-                        </p>
-                        <img
-                            src="/icon.svg"
-                            width="24"
-                            height="24"
-                            alt="project loved"
-                            class="glow"
-                        >
-                    </template>
-                    <template #items>
-                        <Navbar.Item href="/">Submissions</Navbar.Item>
-                        <Navbar.Item href="/consents">Mapper consents</Navbar.Item>
-                        <Navbar.Item href="/team">Team</Navbar.Item>
-                        <Navbar.Item href="/statistics">Statistics</Navbar.Item>
-                    </template>
-                </Navbar.Section>
-
-                <Navbar.Section
-                    :logo-inset="{
-                        left: 10,
-                        top: 32.5,
-                        bottom: 9.5
-                    }"
-                >
-                    <template #logo>
-                        <UserLove class="opacity-50" width="24" height="24" />
-                    </template>
-                    <template #items>
-                        <Navbar.Item href="/admin">Admin</Navbar.Item>
-                    </template>
-                </Navbar.Section>
-            </Navbar.Root>
-
+            <LovedNavbar />
             <RouterView />
             <div>Count is: {{ count }}</div>
             <motion.button
---
+++ packages/website/src/components/LovedNavbar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { UserLove } from "@iconoir/vue";
+import { Navbar } from "@loved/ui";
+
+const isAdmin = ref(false);
+</script>
+
+<template>
+    <Navbar.Root>
+        <Navbar.Section
+            :logo-inset="{
+                left: 15,
+                top: 32.5,
+                bottom: 9.5
+            }"
+        >
+            <template #logo>
+                <p class="text-xl mr tracking-[3px] select-none glow max-lg:hidden">
+                    PROJECT LOVED
+                </p>
+                <img
+                    src="/icon.svg"
+                    width="24"
+                    height="24"
+                    alt="project loved"
+                    class="glow"
+                >
+            </template>
+            <template #items>
+                <Navbar.Item href="/">Submissions</Navbar.Item>
+                <Navbar.Item href="/consents">Mapper consents</Navbar.Item>
+                <Navbar.Item href="/team">Team</Navbar.Item>
+                <Navbar.Item href="/statistics">Statistics</Navbar.Item>
+            </template>
+        </Navbar.Section>
+
+        <Navbar.Section
+            v-if="isAdmin"
+            :logo-inset="{
+                left: 10,
+                top: 32.5,
+                bottom: 9.5
+            }"
+        >
+            <template #logo>
+                <UserLove class="opacity-50" width="24" height="24" />
+            </template>
+            <template #items>
+                <Navbar.Item href="/admin">Admin</Navbar.Item>
+            </template>
+        </Navbar.Section>
+    </Navbar.Root>
+</template>
