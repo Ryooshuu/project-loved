15bb3ecedc8b5eb5b39d74408161dd3e68239228
Ryooshuu <kyoko.laica@gmail.com>
2025-05-27 16:42:31 -0400

    add review status picker

---
+++ packages/website/src/pages/submissions/ReviewStatusPicker.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { TaggedComboBox } from "@loved/ui";
+
+const options = ["Reviewed", "Not reviewed", "Positive", "Neutral", "Negative"];
+</script>
+
+<template>
+    <div class="flex flex-col gap-0.5">
+        <p class="text-col-5">Review status</p>
+        <TaggedComboBox
+            :options="options"
+        />
+    </div>
+</template>
--- packages/website/src/pages/submissions/SubmissionsFilters.vue
+++ packages/website/src/pages/submissions/SubmissionsFilters.vue
@@ -4,6 +4,7 @@ import RulesetSelector from "./RulesetSelector.vue";
 import { useSubmissionsFiltersStore } from "../../stores/submissionsFiltersStore";
 import { storeToRefs } from "pinia";
 import BeatmapStatusPicker from "./BeatmapStatusPicker.vue";
+import ReviewStatusPicker from "./ReviewStatusPicker.vue";
 
 const filterStore = useSubmissionsFiltersStore();
 const { query } = storeToRefs(filterStore);
@@ -25,9 +26,13 @@ const { query } = storeToRefs(filterStore);
                 class="px-2 py-1 rounded-md surface-1 border-2 border-transparent focus:border-surface-2 text-sm placeholder:text-col-4 outline-none transition-all"
             >
         </div>
-        <div class="flex flex-col gap-2.5">
+        <div class="flex flex-col gap-1.5">
             <p>Filters</p>
             <BeatmapStatusPicker />
+            <ReviewStatusPicker />
+        </div>
+        <div class="flex flex-col gap-1.5">
+            <p>Sort by</p>
         </div>
     </BorderedBox>
 </template>
